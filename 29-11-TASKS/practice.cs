9eb1391ed7a11e34282788aec0b59a4343c4bae3
Your Name <you@example.com>
2022-11-29 16:46:26 +0300

    task

---
+++ 29-11-TASKS/practice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        //practice
+        abstract class car
+        {
+            public abstract void StartEngine();
+            public abstract void StopEngine();
+            public abstract void OpenDoor();
+            public abstract void CloseDoor();
+            public abstract void Honk();
+            public abstract void IncreaseSpeed();
+            public abstract void DecreaseSpeed();
+            public abstract void Breaks();
+            public abstract void OpenWindow();
+            public abstract void CloseWindow();
+            public abstract void AdjustSeat();
+            public abstract void CleanGlass();
+            public abstract void ChangeGear();
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
